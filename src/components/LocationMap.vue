d18428f2a4e30bda0c63d3aac9800bacd9760f9e
Shandon Bowden <52051023+shandonb@users.noreply.github.com>
2025-02-18 19:57:36 -0500

    Imported leaflet marker icon

--- src/components/LocationMap.vue
+++ src/components/LocationMap.vue
@@ -10,6 +10,8 @@
 <script lang="ts" setup>
 import { ref, onMounted, defineProps } from 'vue';
 import L from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 const props = defineProps<{
   lat: number;
@@ -18,6 +20,13 @@ const props = defineProps<{
 }>();
 
 const mapContainer = ref<HTMLElement | null>(null);
+const customIcon = L.icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34]
+});
 
 onMounted(() => {
   if (mapContainer.value) {
@@ -28,7 +37,7 @@ onMounted(() => {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    const marker = L.marker([props.lat, props.lon]).addTo(map);
+    const marker = L.marker([props.lat, props.lon], { icon: customIcon }).addTo(map);
     marker.bindPopup(`${props.name}`)
   }
 });
